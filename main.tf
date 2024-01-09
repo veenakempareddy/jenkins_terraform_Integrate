bd223845c0e8e08cfd0bc47f76dc7d7d56002047
veenakempareddy <134626810+veenakempareddy@users.noreply.github.com>
2024-01-09 07:54:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-    region = "eu-north-1" # Desired region for resource creation
+    region = "ap-south-1" # Desired region for resource creation
     access_key = 'AKIAQ6PPDBWXFK4LECZS'  #access key of the user
     secret_key = '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'  #secret key of the user
 }
@@ -8,9 +8,9 @@ provider "aws" {
 
 # Ec2 Creation
 resource "aws_instance" "tefform_instance" {
-    ami = "ami-0287a05f0ef0e9d9a" #ami id using this ec2 will be created
-    instance_type = "t3.micro"    # instance type
-    key_name = "awspemkey"        # Key_name to be used
+    ami = "ami-0d3f444bc76de0a79" #ami id using this ec2 will be created
+    instance_type = "t2.micro"    # instance type
+    key_name = "awskeypair"        # Key_name to be used
     tags = {
         "Name" = "Terraform_ec2"  #tag for the ec2 
     }
